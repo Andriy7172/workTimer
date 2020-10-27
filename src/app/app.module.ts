fcd4b16a8a36b6a89a403e9af711be683817df14
Andrii Zhokh <andriizhokh@gmail.com>
2020-10-27 18:44:47 +0200

    Deleted unused pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SecondsToHourPipe } from './pipes/seconds-to-hour.pipe';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecondsToHourPipe
   ],
   imports: [
     BrowserModule,
